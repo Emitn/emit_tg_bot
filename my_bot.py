3f96a887e246dbd85db8bec49b3bc434ab2ac390
Eugene Mitrokhin <enmitrokhin@gmail.com>
2025-11-30 10:06:26 +0400

    random cat as responce at message

--- my_bot.py
+++ my_bot.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from anyio import sleep
 from dotenv import load_dotenv
 import time
 
@@ -8,6 +7,8 @@ load_dotenv()
 
 API_URL = os.getenv('API_URL')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+API_CATS_URL = "https://api.thecatapi.com/v1/images/search"
+ERROR_TEXT = "NO_CAT"
 
 TEXT = "Nice update"
 
@@ -16,8 +17,9 @@ MAX_COUNTER = 100
 offset: int = -2
 counter: int = 0
 chat_id: int
+cat_response: requests.Response
+cat_link: str
 
-print(requests.get(f"{API_URL}{BOT_TOKEN}/getUpdates?offset=-1"))
 
 while counter < MAX_COUNTER:
     print(f"attempt={counter}")
@@ -27,6 +29,11 @@ while counter < MAX_COUNTER:
         for result in updates["result"]:
             offset = result["update_id"]
             chat_id = result["message"]["from"]["id"]
-            requests.get(f"{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}")
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]["url"]
+                requests.get(f"{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}")
+            else:
+                requests.get(f"{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}")
     time.sleep(1)
     counter += 1
