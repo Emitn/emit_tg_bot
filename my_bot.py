e9f355c48efefd58abdcc3d74346c6711b46d49c
Eugene Mitrokhin <enmitrokhin@gmail.com>
2025-11-30 11:40:48 +0400

    add echo photo and sending photo instead of link

--- my_bot.py
+++ my_bot.py
@@ -1,9 +1,9 @@
 import os
 import requests
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -13,7 +13,7 @@ dp = Dispatcher()
 
 async def process_waifu_command(message: Message):
     waifu_response = requests.get("https://api.waifu.im/search")
-    await message.answer(text=f"{waifu_response.json()['images'][0]['preview_url']}")
+    await message.answer_photo(waifu_response.json()['images'][0]["url"])
 
 async def process_start_command(message: Message):
     await message.answer("Hi, I am echo and i have many cats")
@@ -21,6 +21,8 @@ async def process_start_command(message: Message):
 async def process_help_command(message: Message):
     await message.answer("Send me anything and I'll repeat after you")
 
+async def process_photo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
 async  def process_message(message: Message):
     await message.reply(message.text)
@@ -30,5 +32,6 @@ if __name__ == "__main__":
     dp.message.register(process_start_command, Command(commands="start"))
     dp.message.register(process_help_command, Command(commands="help"))
     dp.message.register(process_waifu_command, Command(commands="waifu"))
+    dp.message.register(process_photo, F.content_type == ContentType.PHOTO)
     dp.message.register(process_message)
     dp.run_polling(bot)
